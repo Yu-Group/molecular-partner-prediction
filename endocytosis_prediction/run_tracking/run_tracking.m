1ffcc0b012629894727a515cadc77ae70c404003
Chandan Singh <chandan_singh@berkeley.edu>
2019-10-22 18:28:54 -0700

    add tracking script

--- endocytosis_prediction/run_tracking/run_tracking.m
+++ endocytosis_prediction/run_tracking/run_tracking.m
@@ -28,4 +28,35 @@ channel 2 name: "EGFP"
 
 % Saves into Detection, Tracking, Analysis folders
 % Visualizing: cmeDataViewer(data)
+% If there are multiple folders, % Visualizing: cmeDataViewer(data(folder_num))
 % Saves tracks for RFP (clathrin) and tracks at RFP locations for GFP (auxilin)
+
+
+%{
+FINAL OUTPUT
+
+Remove outliers? (y/n) n
+
+
+Lifetime analysis - processing: 100%
+Max. intensity threshold on first 4 frames: 1314.53
+95th percentile of 1st frame distribution: 1453.73
+Lifetime distribution percentiles (5th, 25th, 50th, 75th, 95th):
+  CSs:  [NaN, 5.4, 7.7, 13.4, 37.5] s
+  CCPs: [5.7, 22.9, 45.8, 86.9, 198.3] s
+Error using griddedInterpolant
+The coordinates of the input points must be finite values; Inf and NaN
+are not permitted.
+
+Error in interp1 (line 149)
+        F = griddedInterpolant(X,V,method);
+
+Error in runLifetimeAnalysis (line 426)
+        lftPctCS = interp1(lftCDF(uidx), lftRes.t(uidx), [0.05 0.25 0.5
+        0.75 0.95]);
+
+Error in cmeAnalysis (line 151)
+    res.lftRes = runLifetimeAnalysis(data, lopts{:},...
+ 
+
+%}
