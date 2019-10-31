41b846eaeb6a1a782adffd74255c7cfebfe36f8b
Chandan Singh <chandan_singh@berkeley.edu>
2019-10-31 11:23:59 -0700

    add random over sampling

--- endocytosis_prediction/data_tracks.py
+++ endocytosis_prediction/data_tracks.py
@@ -43,19 +43,31 @@ def get_tracks(fname = '/scratch/users/vision/data/abc_data/auxilin_data_tracked
 def preprocess(df):
     df = df[df.len > 2]
     df['X_max'] = np.array([max(x) for x in df.X.values])
+    df['X_mean'] = np.nan_to_num(np.array([np.nanmean(x) for x in df.X.values]))
+    df['X_std'] = np.nan_to_num(np.array([np.std(x) for x in df.X.values]))
     df['Y_max'] = np.array([max(y) for y in df.Y.values])    
     df['Y_mean'] = np.nan_to_num(np.array([np.nanmean(y) for y in df.Y.values]))
     df['Y_std'] = np.nan_to_num(np.array([np.std(y) for y in df.Y.values]))
     return df
 
 def add_outcome(df, thresh=3.25):
-    '''Add binary outcome of whether spike happened
+    '''Add binary outcome of whether spike happened and info on whether events were questionable
     '''
     df['outcome_score'] = df['Y_max'].values - (df['Y_mean'].values + thresh * df['Y_std'].values)
     df['outcome'] = (df['outcome_score'].values > 0).astype(np.int) # Y_max was big
+    
+    
+    # multiple events (at the same location)
+    df['multiple'] = None
+    
+    # partial events (signal is already high at the start)
+    df['partial'] = None
+    
     return df
 
 def extract_X_mat(df, p=300):
+    '''Extract matrix for X filled with zeros after sequences end
+    '''
     n = df.shape[0]
     X_mat = np.zeros((n, p)).astype(np.float32)
     X = df['X'].values
