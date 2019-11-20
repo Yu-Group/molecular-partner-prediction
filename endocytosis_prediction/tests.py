6120e0eea42153502b9fb09d5210f8bb3ed65ae9
Chandan Singh <chandan_singh@berkeley.edu>
2019-11-20 08:57:16 -0800

    add test

---
+++ endocytosis_prediction/tests.py
@@ -0,0 +1,23 @@
+import numpy as np
+import os
+from os.path import join as oj
+import numpy as np
+import mat4py
+import pandas as pd
+import data_tracks
+import models
+import pickle as pkl
+from style import *
+
+
+def test_pipeline():
+    '''test that the data pipeline succesfully completes
+    '''
+    print('testing pipeline...')
+    df = data_tracks.get_data()
+    assert(df.lifetime.max() < 300)
+    
+if __name__ == '__main__':
+    test_pipeline()
+    print('all tests passed!')
+    
