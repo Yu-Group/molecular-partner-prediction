9aab02294338da0efce582c87b8accd37687b581
Xiao Li <xsli@arwen.Berkeley.EDU>
2020-03-12 16:39:10 -0700

    variance after trend filtering

--- trend_filtering.py
+++ trend_filtering.py
@@ -11,8 +11,10 @@ def trend_filtering(y, vlambda, order=1):
         D = scipy.sparse.spdiags(np.vstack((e, -2*e, e)), range(3), n-2, n)
     elif order == 2:
         D = scipy.sparse.spdiags(np.vstack((e, -3*e, 3*e, -e)), range(4), n-3, n)
+    elif order == 3:
+        D = scipy.sparse.spdiags(np.vstack((e, -4*e, 6*e, -4*e, e)), range(5), n-4, n)
     else:
-        raise Exception("order can only be 1 or 2")
+        raise Exception("order can only be 1-3")
     
     x = cp.Variable(shape=n)
     obj = cp.Minimize(0.5 * cp.sum_squares(y - x)
