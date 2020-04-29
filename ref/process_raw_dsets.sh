2317ed2a53b3cff0d4b22e19897cb606a7ad391d
Chandan Singh <chandan_singh@berkeley.edu>
2020-04-28 17:35:00 -0700

    configure for more dsets

--- ref/process_raw_dsets.sh
+++ ref/process_raw_dsets.sh
@@ -1,3 +1,9 @@
 DIR_WITH_ZIPS=/scratch/users/vision/data/abc_data/auxilin_new_data
 cd $DIR_WITH_ZIPS
-unzip "*.zip"
+# unzip "*.zip"
+ls $DIR_WITH_ZIPS
+
+UNZIPPED_DIR=$DIR_WITH_ZIPS/AI_Clathrin_molecularPrediction
+ls $UNZIPPED_DIR
+
+
