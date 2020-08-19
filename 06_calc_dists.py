feed0dba81dd5df2fefbed49f234a1aabc5b7dce
Chandan Singh <chandan_singh@berkeley.edu>
2020-08-18 19:29:15 -0700

    add NN DTW plots

--- 06_calc_dists.py
+++ 06_calc_dists.py
@@ -13,28 +13,19 @@ from matplotlib_venn import venn3, venn2
 import viz
 import config
 from tqdm import tqdm
-outcome_def = 'y_consec_thresh'
-
-df = data.get_data()
-df = df[df['valid'] == 1] # exclude test cells, short/long tracks, hotspots
-viz.print_metadata()
-
-def sort_outcome(df, outcome_def):
-    outcome_score = df['Y_max'].values
-    idxs_sort = np.argsort(outcome_score)
-    return df[outcome_def].values[idxs_sort], df['Y'].values[idxs_sort]
-
-outcome_sort, Y_sort = sort_outcome(df, outcome_def)
-
 from scipy.spatial.distance import euclidean
 
 from fastdtw import fastdtw
 
 
+outcome_def = 'y_consec_thresh'
+
+df = data.get_data()
+df = df[df['valid'] == 1] # exclude test cells, short/long tracks, hotspots
 n = df.shape[0]
 dists = np.zeros((n, n))
 for i in tqdm(range(n)):
-    for j in range(n - i):
+    for j in range(i + 1, n):
         x0 = np.array(df['X'].iloc[i])
         x1 = np.array(df['X'].iloc[j])
         distance, path = fastdtw(x0, x1, dist=euclidean)
